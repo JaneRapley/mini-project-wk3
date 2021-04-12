f0e7ff3e2683010d6203e5fd11f6fae51403cc0f
JaneyR <rapley.jane@gmail.com>
2021-04-12 15:49:43 -0400

    moved files from class repository to local file location to work through activity on my own

---
+++ JRREADME.md
@@ -0,0 +1,21 @@
+# mini-project-wk3
+Pseudo Code
+ 1. Prompt user for R/P/S -- look up how to prompt the user
+ 2. Randomly select a computer R/P/S -- check the bonus from 26
+ 3. Compare user response to computer selection -- conditional
+    look up how to alert user of what is happening
+ 4. Update and display statistics -- 
+    create separate variables (wins, losses, ties)
+    cretae an object (statistics = wins: 0, losses: 0, ties: 0)
+ 5. Confirm if want to play again
+    look up how to confrim this
+
+ I need to use code that I can re-use
+    all of my logic is in a FUNCTION
+
+everything will happen in the function
+function playGame ( ) {
+
+}
+
+playGame ();
---
+++ script.js
@@ -0,0 +1,22 @@
+//function playGame(){
+    //1 prompt the user for R/P/S -- look up how to prompt the user
+    //2 randomly select a computer R/P/S -- check the bonus from 26
+    //3 compare user response to computer seletion -- condational
+        //look up how to alert the user of what is happening
+    //4update and dispaly the statistics
+        //create separate variable (wins, losses, ties)
+        //create an object (stats = {wins: 0 losses: 0 ties: 0})
+    //5confirm if want to play again
+        //look up how to confirm this
+//}
+ //playGame();
+
+
+ function playGame (){
+    window.alert("Do you want to play a Game?");
+    var seletion = window.prompt("Type R, P, or S");
+    var compSelect = ("R", "P", "S")       
+ }
+
+playGame()
+
